8fbe843697c7466e3290f3e7f5cea272adf7cd90
Ga Jin <gajinkee@gail.com>
2019-09-02 19:07:44 +0800

    first push

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    problems: [
+        {
+            userHandle: "user",
+            body:"this is the problem body",
+            createdAt:"2019-09-02T01:23:50.656Z",
+            likeCount:5,
+            comentCount: 2,
+        }
+    ]
+};
---
+++ functions/index.js
@@ -0,0 +1,55 @@
+const functions = require('firebase-functions');
+const admin =require('firebase-admin');
+
+admin.initializeApp();
+
+const express=require("express");
+const app= express();
+
+app.get('/problems',(req,res)=> {
+  admin
+    .firestore()
+    .collection("problems")
+    .orderBy("createdAt",'desc')
+    .get()
+    .then((data) => {
+      let problems = [];
+      data.forEach((doc) => {
+        problems.push({
+          problemId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+
+        });
+      });
+      return res.json(problems);
+    })
+   .catch(err => console.error(err));
+});
+
+app.post('/problem', (req,res) => {
+
+  const newProblem = {  
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection('problems')
+    .add(newProblem)
+    .then((doc) => {
+      res.json({message: 'document successful'})
+    })
+
+    .catch((err) =>{
+      res.status(500).json({error:"Theres an error"})
+      console.error(err);
+    });
+});
+
+
+
+exports.api = functions.https.onRequest(app);
