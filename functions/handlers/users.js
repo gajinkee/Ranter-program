003fc069159b66cffe10c360964103b0a7f55c9e
Ga Jin <gajinkee@gail.com>
2019-11-10 21:25:26 +0800

    ok 2:27:13
    all good finished image upload.
    adding xtra user details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,4 +1,4 @@
-const {db} = require('../util/admin');
+const {admin,db} = require('../util/admin');
 
 const config=require('../util/config');
 
@@ -7,7 +7,7 @@ require('../node_modules/firebase/firebase-auth');
 
 firebase.initializeApp(config);
 
-const { validateSignupData, validateLoginData} = require('../util/validators');
+const { validateSignupData, validateLoginData, reduceUserDetails} = require('../util/validators');
 
 exports.signup =  (req, res) => {
     const newUser = {
@@ -20,6 +20,8 @@ exports.signup =  (req, res) => {
     const {valid, errors} = validateSignupData(newUser);
 
     if(!valid) return res.status(400).json(errors);
+
+    const noImg ="no-img.png"
   
     
   
@@ -47,6 +49,7 @@ exports.signup =  (req, res) => {
           handle: newUser.handle,
           email: newUser.email,
           createdAt: new Date().toISOString(),
+          imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
           userId
         };
         return db.doc(`/users/${newUser.handle}`).set(userCredentials);
@@ -90,4 +93,72 @@ exports.login =  (req, res) => {
           return res.status(403).json({general: 'Wrong credentials, please try again'});
         } else return res.status(500).json({ error: err.code });
       });
+  }
+//user details added
+exports.addUserDetails = (req,res) =>{
+  let userDetails = reduceUserDetails(req.body);
+
+  db.doc(`/users/${req.user.handle}`).update(userDetails)
+  .then (()=>{
+    return res.json({message: 'details added done'});
+  })
+  .catch(err=>{
+    console.error(err);
+    return res.status(500).json({error: err.code})
+  })
+}
+
+//image upload
+ exports.uploadImage =( req,res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os= require ('os');
+    const fs = require('fs');
+
+    const busboy=new BusBoy({headers: req.headers});
+
+    let imageFileName;
+    let imageToBeUploaded= {};
+
+    busboy.on('file',(fieldname,file,filename, encoding ,mimetype) =>{
+      if (mimetype!=='image/jpeg' && mimetype !== 'image/png'){
+        return res.status(400).json({error: 'wrong file type submitted'})
+
+      }
+      console.log(fieldname,filename,encoding,mimetype);
+      
+     
+      const imageExtension= filename.split(".")[filename.split(".").length -1];
+      imageFileName= `${Math.round(Math.random()*1000000000000).toString()}.${imageExtension}`;
+      const filepath = path.join(os.tmpdir(),imageFileName);
+      imageToBeUploaded= { filepath, mimetype};
+      
+
+      file.pipe(fs.createWriteStream(filepath));
+    });
+    busboy.on('finish',()=>{
+      admin.storage().bucket().upload(imageToBeUploaded.filepath,{
+        resumable: false,
+        metadata: {
+          metadata:{
+            contentType: imageToBeUploaded.mimetype
+          }
+        }
+      })
+      
+      .then(()=>{
+        const imageUrl= `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
+        return db.doc(`/users/${req.user.handle}`).update({imageUrl});
+        
+      })
+      .then(() => {
+        return res.json({message: 'image upload succeess'});
+      })
+      .catch(err =>{
+        console.error(err);
+        return res.status(500).json({error: err.code});
+
+      });
+    });
+    busboy.end(req.rawBody);
   }
--- functions/index.js
+++ functions/index.js
@@ -5,14 +5,17 @@ const app = require("express")();
 const FBAuth= require('./util/fbAuth');
 
 const {getAllProblems, postOneProblem }= require('./handlers/problems');
-const {signup, login } = require('./handlers/users');
+const {signup, login , uploadImage,addUserDetails} = require('./handlers/users');
 
 //problem route
 app.get('/problems',getAllProblems );
 app.post("/problem", FBAuth, postOneProblem );
+app.post('/user/image',FBAuth, uploadImage)
+app.post('/user',FBAuth,addUserDetails)
 
 //user route
 app.post("/signup",signup);
 app.post('/login',login);
+
 
 exports.api = functions.https.onRequest(app);
